8ba9eeb5419ab52a3862342e6669e7ed2fb20a84
sgovind158 <govindsahu2842@gmail.com>
2022-10-27 14:26:04 +0530

    creating car class

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ class Person {
     return this.name;
   }
 }
+
+
+
+
+// Creating one Car class and inherit by person
+class Car extends Person {
+    // #carName is private
+    #carName;
+    constructor(person, carName) {
+      super(person);
+      this.#carName = carName;
+    }
+  
+    // This method print car name
+    printCarName() {
+      console.log(`This is my car name ${this.#carName}`);
+    }
+  
+    //This method print manufacturing details
+    manufacturing() {
+      console.log("It is new and made in india");
+    }
+  
+    //This method print fuel type
+    fuel() {
+      console.log("It runs on diesel");
+    }
+  }
